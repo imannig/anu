3da92525183f3647baf31f51eb7409e130a91da8
Zahran Ansaria <akugedekntl@gmail.com>
2022-04-02 07:37:08 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -3,6 +3,7 @@ cd ~/rom
 
 . build/envsetup.sh
 export BUILD_USERNAME=imannig
+export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_HOSTNAME=cirrus-kontol
 export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
